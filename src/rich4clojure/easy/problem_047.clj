003d4581790e0a2f390bf93eca1c5055ded26db7
chenj7 <chenj7@gmail.com>
2021-09-08 17:17:31 -0400

    easy_47: Add test conditions

--- src/rich4clojure/easy/problem_047.clj
+++ src/rich4clojure/easy/problem_047.clj
@@ -17,10 +17,10 @@
   )
 
 (tests
-  #{4 5 6} := __
-  [1 1 1 1 1] := __
-  {4 :a 2 :b} := __
-  (contains? [1 2 4] __) :=)
+  (contains? #{4 5 6} __) := true
+  (contains? [1 1 1 1 1] __) := true
+  (contains? {4 :a 2 :b} __) := true
+  (contains? [1 2 4] __) := false)
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/cbcede67c0c350f7d800b078a5c0f525
